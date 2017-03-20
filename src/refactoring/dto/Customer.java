f977a851b97928d15b386bb387d45ff97d93266e
osram21 <osram21@naver.com>
2017-03-20 11:47:18 +0900

    고객 요구사항 추가....(html결과물) 기능적 재사용 불가(ctrl+c ctrl+v)

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -28,25 +28,26 @@ public class Customer {
 		
 		for(Rental each : rentals){
 			double thisAmount = 0;
-			// 비디오물당 대여료 1. 일반물 (2일) 2000원, 일일초과 1500원 , 적립1
-						//2. 아동물 (3일) 1500원, 일일초과 1500원, 적립1
-						//3. 최신물 (1일) 3000원, 일일초과 3000원, 적립1
+			// 비디오물당 대여료 1. 일반물 (2일) 2000원, 일일초과 1500원 , 적립1 ->(2일, 3000) 일일초과 2000, 적립 1
+						//2. 아동물 (3일) 1500원, 일일초과 1500원, 적립1 ->(2일, 2000) 일일초과 1500, 적립1
+						//3. 최신물 (1일) 3000원, 일일초과 3000원, 적립1 ->(2일, 4000) 일일초과 4000, 적립1+1
+						//HTML형식을 출력 원함
 			Movie movie = each.getMovie();
 			
 			int pirceCode = movie.getPriceCode();
 			int daysRented = each.getDaysRented(); //해당 비디오물의 대여기간
 			switch(pirceCode){
 			case Movie.ReGULAR:
-				thisAmount = 2000;
+				thisAmount = 3000;
 				if (daysRented > 2) {
-					thisAmount += (daysRented - 2) * 1500;
+					thisAmount += (daysRented - 2) * 2000;
 				}
 				break;
 			case Movie.NEW_RELEASE:
-				thisAmount = (daysRented * 3000);
+				thisAmount = (daysRented * 4000);
 				break;
 			case Movie.CHLDRENS:
-				thisAmount = 1500;
+				thisAmount = 2000;
 				if (daysRented > 3) {
 					thisAmount += (daysRented - 3) * 1500;
 				}
@@ -62,6 +63,54 @@ public class Customer {
 			//for loop
 		}
 		sb.append(String.format("누적대여료: %s%n적립포인트 : %s%n", totalAmount, frequentRenterPoints));
+		return sb.toString();
+	}
+	
+	public String HtmlStatement() {
+		double totalAmount = 0;// 대여료
+		int frequentRenterPoints = 0;// 적립포인트
+		StringBuilder sb = new StringBuilder();
+		sb.append("<h1><em>" + getName() + "고객님의 대여기록</em></h1><br>");
+
+		for (Rental each : rentals) {
+			double thisAmount = 0;
+			// 비디오물당 대여료 1. 일반물 (2일) 2000원, 일일초과 1500원 , 적립1 ->(2일, 3000) 일일초과
+			// 2000, 적립 1
+			// 2. 아동물 (3일) 1500원, 일일초과 1500원, 적립1 ->(2일, 2000) 일일초과 1500, 적립1
+			// 3. 최신물 (1일) 3000원, 일일초과 3000원, 적립1 ->(2일, 4000) 일일초과 4000, 적립1+1
+			// HTML형식을 출력 원함
+			Movie movie = each.getMovie();
+
+			int pirceCode = movie.getPriceCode();
+			int daysRented = each.getDaysRented(); // 해당 비디오물의 대여기간
+			switch (pirceCode) {
+			case Movie.ReGULAR:
+				thisAmount = 3000;
+				if (daysRented > 2) {
+					thisAmount += (daysRented - 2) * 2000;
+				}
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount = (daysRented * 4000);
+				break;
+			case Movie.CHLDRENS:
+				thisAmount = 2000;
+				if (daysRented > 3) {
+					thisAmount += (daysRented - 3) * 1500;
+				}
+				break;
+			}
+			frequentRenterPoints++;
+			if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1) {
+				frequentRenterPoints++;
+			}
+			sb.append(String.format("nsbp;nsbp;%s nsbp;nsbp; %s<br>", each.getMovie().getTitle(), thisAmount));
+
+			totalAmount += thisAmount;
+			// for loop
+		}
+		sb.append(
+				String.format("<p>누적대여료: <em>%s<em><br><p>적립포인트 : <em>%s</em><br>", totalAmount, frequentRenterPoints));
 		return sb.toString();
 	}
 }
