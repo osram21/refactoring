e1064fd295f3b29c6e71b486986b11111a345b0f
osram21 <osram21@naver.com>
2017-03-20 12:30:51 +0900

    메서드 이동 기법 적용

--- src/refactoring/dto/Rental.java
+++ src/refactoring/dto/Rental.java
@@ -18,5 +18,29 @@ public class Rental {
 		return daysRented;
 	}
 	
+	public double getCharge(){
+		double result = 0;
+		// 비디오물당 대여료 1. 일반물 (2일) 2000원, 일일초과 1500원 , 적립1
+					//2. 아동물 (3일) 1500원, 일일초과 1500원, 적립1
+					//3. 최신물 (1일) 3000원, 일일초과 3000원, 적립1
 	
+		switch(movie.getPriceCode()){
+		case Movie.ReGULAR:
+			result = 2000;
+			if (daysRented > 2) {
+				result += (daysRented - 2) * 1500;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			result = (daysRented * 3000);
+			break;
+		case Movie.CHLDRENS:
+			result = 1500;
+			if (daysRented > 3) {
+				result += (daysRented - 3) * 1500;
+			}
+			break;
+		}
+		return result;
+	}
 }
