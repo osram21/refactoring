0df5f596e8765f026157e994281c762ae81c19ec
osram21 <osram21@naver.com>
2017-03-20 12:20:09 +0900

    메서드 추출적용 완료

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -3,6 +3,8 @@ package refactoring.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.omg.PortableServer.ThreadPolicyOperations;
+
 public class Customer {
 	private String name;
 	private List<Rental> rentals;
@@ -27,31 +29,7 @@ public class Customer {
 		sb.append(getName()+"고객님의 대여기록\n");
 		
 		for(Rental each : rentals){
-			double thisAmount = 0;
-			// 비디오물당 대여료 1. 일반물 (2일) 2000원, 일일초과 1500원 , 적립1
-						//2. 아동물 (3일) 1500원, 일일초과 1500원, 적립1
-						//3. 최신물 (1일) 3000원, 일일초과 3000원, 적립1
-			Movie movie = each.getMovie();
-			
-			int pirceCode = movie.getPriceCode();
-			int daysRented = each.getDaysRented(); //해당 비디오물의 대여기간
-			switch(pirceCode){
-			case Movie.ReGULAR:
-				thisAmount = 2000;
-				if (daysRented > 2) {
-					thisAmount += (daysRented - 2) * 1500;
-				}
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount = (daysRented * 3000);
-				break;
-			case Movie.CHLDRENS:
-				thisAmount = 1500;
-				if (daysRented > 3) {
-					thisAmount += (daysRented - 3) * 1500;
-				}
-				break;
-			}
+			double thisAmount = amountFor(each);
 			frequentRenterPoints++;
 			if(each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() >1){
 				frequentRenterPoints++;
@@ -63,5 +41,33 @@ public class Customer {
 		}
 		sb.append(String.format("누적대여료: %s%n적립포인트 : %s%n", totalAmount, frequentRenterPoints));
 		return sb.toString();
+	}
+	
+	private double amountFor(Rental aRental){
+		double result = 0;
+		// 비디오물당 대여료 1. 일반물 (2일) 2000원, 일일초과 1500원 , 적립1
+					//2. 아동물 (3일) 1500원, 일일초과 1500원, 적립1
+					//3. 최신물 (1일) 3000원, 일일초과 3000원, 적립1
+		Movie movie = aRental.getMovie();
+		int pirceCode = movie.getPriceCode();
+		int daysRented = aRental.getDaysRented(); //해당 비디오물의 대여기간
+		switch(pirceCode){
+		case Movie.ReGULAR:
+			result = 2000;
+			if (daysRented > 2) {
+				result += (daysRented - 2) * 1500;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			result = (daysRented * 3000);
+			break;
+		case Movie.CHLDRENS:
+			result = 1500;
+			if (daysRented > 3) {
+				result += (daysRented - 3) * 1500;
+			}
+			break;
+		}
+		return result;
 	}
 }
