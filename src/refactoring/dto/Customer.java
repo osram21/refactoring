435f09d9fa7f8eed965a3453f2f8292c1bc62265
osram21 <osram21@naver.com>
2017-03-21 10:40:13 +0900

    임시변수메서드호출변환 getTotalCharge(), getFre 완

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -1,42 +1,49 @@
 package refactoring.dto;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import org.omg.PortableServer.ThreadPolicyOperations;
-
 public class Customer {
-	private String name;
-	private List<Rental> rentals;
-	
-	public Customer(String name) {
-		this.name = name;
-		this.rentals = new ArrayList<>();
-	}
+   private String name;
+   private List<Rental> rentals;
+   
+   public Customer(String name) {
+      this.name = name;
+      this.rentals = new ArrayList<>();
+   }
 
-	public String getName() {
-		return name;
-	}
-	
-	public void addRental(Rental aRental){
-		this.rentals.add(aRental);
-	}
-	
-	public String statement(){
-		double totalAmount = 0;//대여료
-		int frequentRenterPoints = 0;//적립포인트
-		StringBuilder sb = new StringBuilder();
-		sb.append(getName()+"고객님의 대여기록\n");
-		
-		for (Rental each : rentals) {
-			frequentRenterPoints += each.getfrequentRenterPoints();
-			
-			sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(), each.getCharge()));
+   public String getName() {
+      return name;
+   }
+   
+   public void addRental(Rental aRental){
+      this.rentals.add(aRental);
+   }
+   
+   public String statement(){
+      StringBuilder sb = new StringBuilder();
+      sb.append(getName()+"고객님의 대여기록\n");
+      
+      for(Rental each : rentals){
+         sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(), each.getCharge()));
+      }//for loop
+      
+      sb.append(String.format("누적대여료: %s%n적립포인트 : %s%n", getTotalCharge(), getFrequentRenterPoints()));
+      return sb.toString();
+   }
 
-			totalAmount += each.getCharge();
-			// for loop
-		}
-		sb.append(String.format("누적대여료: %s%n적립포인트 : %s%n", totalAmount, frequentRenterPoints));
-		return sb.toString();
-	}
+   private int getFrequentRenterPoints() {
+      int frequentRenterPoints = 0;//적립포인트
+      for(Rental each : rentals){
+         frequentRenterPoints += each.getfrequentRenterPoints();
+      }
+      return frequentRenterPoints;
+   }
+
+   private double getTotalCharge() {
+      double result = 0.0;//대여료
+      for(Rental each : rentals){
+         result += each.getCharge();
+      }
+      return result;
+   }
 }
