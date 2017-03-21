b57a7de0b641a8a7a8791243aeb64813fe723a9e
osram21 <osram21@naver.com>
2017-03-21 10:04:49 +0900

    메서드 추출 적용 후, 시퀀스 다이어그램 추가

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -28,21 +28,15 @@ public class Customer {
 		StringBuilder sb = new StringBuilder();
 		sb.append(getName()+"고객님의 대여기록\n");
 		
-		for(Rental each : rentals){
+		for (Rental each : rentals) {
+			frequentRenterPoints += each.getfrequentRenterPoints();
 			
-			
-			frequentRenterPoints++;
-			if(each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() >1){
-				frequentRenterPoints++;
-			}
 			sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(), each.getCharge()));
-			
+
 			totalAmount += each.getCharge();
-			//for loop
+			// for loop
 		}
 		sb.append(String.format("누적대여료: %s%n적립포인트 : %s%n", totalAmount, frequentRenterPoints));
 		return sb.toString();
 	}
-	
-	
 }
--- src/refactoring/dto/Rental.java
+++ src/refactoring/dto/Rental.java
@@ -43,4 +43,13 @@ public class Rental {
 		}
 		return result;
 	}
+	
+	public int getfrequentRenterPoints() {
+		if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
+			return 2;
+		}else{
+			return 1;
+		}
+		
+	}
 }
